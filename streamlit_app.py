8f364dbf152ce67fb9f6caa458ae26812129ee00
josedjaykv <josedjaykv@gmail.com>
2024-08-19 13:55:56 -0500

    chore: Refactor input features in machine learning app

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,24 +28,28 @@ with st.expander('Data visualization'):
 # Data preparations
 with st.sidebar:
     st.header('Input Features')
-    # "species","","","","","",""
-    island = st.selectbox(
-        'Island',
-        (X.island.unique()), 
-        index=None,
-        placeholder='Select an Island'
-    )
-    gender = st.selectbox(
-        'Gender',
-        (X.sex.unique()),
-        index=None,
-        placeholder='Select a gender'
-    )
+    island = st.selectbox('Island', (X.island.unique()))
     bill_length_mm = st.slider('Bill length (mm)', 30.0, 60.0, 43.9, step=0.05)
     bill_depth_mm = st.slider('Bill depth (mm)', 10.0, 30.0, 17.2, step=0.05)
     flipper_length_mm = st.slider('Flipper length (mm)', 160.0, 240.0, 200.2, step=0.05)
     body_mass_g = st.slider('Body mass (mm)', 2700.0, 6300.0, 4200.0, step=0.05)
+    gender = st.selectbox('Gender', (X.sex.unique()))
 
+    # Create a Data Frame for the input features
+    data = {'island': island,
+            'bill_length_mm': bill_length_mm,
+            'bill_depth_mm': bill_depth_mm,
+            'flipper_length_mm': flipper_length_mm,
+            'body_mass_g': body_mass_g,
+            'sex': gender}
+    input_df = pd.DataFrame(data, index=[0])
+    input_penguins = pd.concat([input_df, X], axis=0)
+
+with st.expander('Input Features'):
+    st.write('**Input penguin**')
+    input_df
+    st.write('**Combined penguin data**')
+    input_penguins
 
 
 
