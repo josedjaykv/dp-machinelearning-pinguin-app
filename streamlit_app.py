952a982664525e630e9e7487795bee07f66b671b
josedjaykv <josedjaykv@gmail.com>
2024-08-19 11:42:04 -0500

    show df

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,4 @@ st.title('🤖 Machine Learning App')
 st.info('This is app builds a machine learning model!')
 
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
