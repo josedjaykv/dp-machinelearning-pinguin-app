720a7fbfe26782d26ecc50aebf918b2ae821c24c
josedjaykv <josedjaykv@gmail.com>
2024-08-19 20:41:57 -0500

    prueba

--- streamlit_app.py
+++ streamlit_app.py
@@ -132,6 +132,7 @@ st.dataframe(df_prediction_proba,
              }, hide_index=True)
 
 #df_prediction_proba
+st.write('Hola')
 penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
 st.success(str(penguins_species[prediction][0]))
 
