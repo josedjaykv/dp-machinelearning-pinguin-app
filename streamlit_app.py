809e34316216e5501ca23108282c7ca899a9635b
josedjaykv <josedjaykv@gmail.com>
2024-08-19 20:37:11 -0500

    Finished ahora si

--- streamlit_app.py
+++ streamlit_app.py
@@ -131,7 +131,7 @@ st.dataframe(df_prediction_proba,
                  ),
              }, hide_index=True)
 
-df_prediction_proba
+#df_prediction_proba
 penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
 st.success(str(penguins_species[prediction][0]))
 
